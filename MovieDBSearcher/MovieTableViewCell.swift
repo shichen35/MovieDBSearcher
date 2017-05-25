50bed498054a986c862f160ac59bdb1a51675dcf
Chen Shi <shichen35@gmail.com>
2017-05-25 11:38:58 -0400

    style update

--- MovieDBSearcher/MovieTableViewCell.swift
+++ MovieDBSearcher/MovieTableViewCell.swift
@@ -9,33 +9,33 @@
 import UIKit
 
 class MovieTableViewCell: UITableViewCell {
-    
-    @IBOutlet weak var imgPoster: UIImageView!
-    @IBOutlet weak var lblTitle: UILabel!
-    @IBOutlet weak var lblReleaseDate: UILabel!
-    @IBOutlet weak var lblOverview: UILabel!
-    @IBOutlet weak var lblRating: UILabel!
-    @IBOutlet weak var lblGenre: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
+  
+  @IBOutlet weak var imgPoster: UIImageView!
+  @IBOutlet weak var lblTitle: UILabel!
+  @IBOutlet weak var lblReleaseDate: UILabel!
+  @IBOutlet weak var lblOverview: UILabel!
+  @IBOutlet weak var lblRating: UILabel!
+  @IBOutlet weak var lblGenre: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+  
+  func display(movie : MovieObject) {
+    let url = URL(string: "https://image.tmdb.org/t/p/w92\(movie.posterUrl)")!
+    imgPoster.kf.setImage(with: url, placeholder: UIImage(named:"no-image.png"), options: nil, progressBlock: nil, completionHandler: nil)
+    lblTitle.text = movie.title
+    lblReleaseDate.text = movie.releaseDate
+    lblOverview.text = movie.overview
+    lblRating.text = String(format: "%.1f", movie.score)
+    var genreStr = String()
+    for genre in movie.genre {
+      genreStr.append(genreStr.characters.count == 0 ? genre : ", \(genre)")
     }
-    
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-    }
-    
-    func display(movie : MovieObject) {
-        let url = URL(string: "https://image.tmdb.org/t/p/w92\(movie.posterUrl)")!
-        imgPoster.kf.setImage(with: url, placeholder: UIImage(named:"no-image.png"), options: nil, progressBlock: nil, completionHandler: nil)
-        lblTitle.text = movie.title
-        lblReleaseDate.text = movie.releaseDate
-        lblOverview.text = movie.overview
-        lblRating.text = String(format: "%.1f", movie.score)
-        var genreStr = String()
-        for genre in movie.genre {
-            genreStr.append(genreStr.characters.count == 0 ? genre : ", \(genre)")
-        }
-        lblGenre.text = genreStr
-    }
+    lblGenre.text = genreStr
+  }
 }
